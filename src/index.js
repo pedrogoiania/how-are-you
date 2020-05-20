4e6ea247ae6f9b02206d240a6b1228a09d504065
Pedro Paulo Duarte Silva <pedro.paulods@hotmail.com>
2020-05-20 00:27:25 -0300

    feat(start): initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { View, Text } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => (
+  <NavigationContainer>
+    <View>
+      <Text>Opa</Text>
+    </View>
+  </NavigationContainer>
+);
+
+export default App;
