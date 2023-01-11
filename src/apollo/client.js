d07871c10685cbae830c66bf6173c7110868bc0d
Pedro Goiânia <pedro.paulods@hotmail.com>
2023-01-11 15:27:19 -0300

    fix(ios): removed flipper to works fine on rn 0.62

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -3,7 +3,7 @@ import ApolloClient from 'apollo-boost';
 import headers from './headers';
 
 const client = new ApolloClient({
-  uri: 'https://howareyou.8020digital.com.br',
+  uri: 'http://localhost:3004/',
   headers,
 });
 
